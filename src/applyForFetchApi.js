c0f38da9497511df5a9786f938cd4d2c01707a7d
Raship Shah <shahraship@gmail.com>
2023-11-08 21:36:23 -0800

    simplified fetch logic for better error handling

--- src/applyForFetchApi.js
+++ src/applyForFetchApi.js
@@ -11,34 +11,27 @@ export const applyForFetchAPi = function(configuration) {
     /*  Store the old fetch api  */
     const OLD_FETCH = fetch;
     /*  Calling our callbacks   */
-    fetch = function () {
+    fetch = async function() {
         try {
             sender.setSenderInstance(OLD_FETCH);
             dispatchPreSubscribers(arguments, sender, configuration.reportOnError);
+            let [resource, config] = arguments;
+            const result = await OLD_FETCH(resource, config);
+            setIsAlreadyApplied(true);
+            if (result && result.clone) {
+                const playableResult = Promise.resolve(result.clone());
+                dispatchPostSubscribers(playableResult, sender, configuration.reportOnError);
+            } else if (result && !result.ok) {
+                return Promise.reject(result);
+            }
+            return result;
         } catch (error) {
             reportConfigOrDefault({
                 sender,
                 error,
                 reportOnError:configuration.reportOnError
             });
+            return Promise.reject(error);
         }
-        /*  calling the old fetch api   */
-        const callOldFetch = OLD_FETCH.apply(this, arguments);
-        callOldFetch.then(function (result) {
-            /*  avoid chaining the original result  */
-            const playableResult = Promise.resolve(result.clone());
-            try {
-                sender.setSenderInstance(callOldFetch);
-                dispatchPostSubscribers(playableResult, sender, configuration.reportOnError);
-            } catch (error) {
-                reportConfigOrDefault({
-                    sender,
-                    error,
-                    reportOnError:configuration.reportOnError
-                });
-            }
-        })
-        setIsAlreadyApplied(true);
-        return callOldFetch;
     };
 }
